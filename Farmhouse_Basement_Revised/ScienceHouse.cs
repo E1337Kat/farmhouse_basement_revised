4a84f6bf501b04dce48c8d86557a1f669977ca94
Ellie Peterson <ekp2h92@gmail.com>
2018-06-06 23:42:46 -0400

    Forgot latest changes in ScienceHouse.cs. More implementation and documentation.

--- Farmhouse_Basement_Revised/ScienceHouse.cs
+++ Farmhouse_Basement_Revised/ScienceHouse.cs
@@ -1,6 +1,7 @@
 ﻿using StardewValley;
 using StardewModdingAPI;
 using xTile.Dimensions;
+using System;
 
 namespace Farmhouse_Basement_Revised
 {
@@ -38,6 +39,9 @@ namespace Farmhouse_Basement_Revised
             mod.Monitor.Log("Loading custom ScienceHouse.", LogLevel.Debug);
         }
 
+        /// <summary>
+        /// I dunno.
+        /// </summary>
         protected override void initNetFields()
         {
             base.initNetFields();
@@ -66,11 +70,20 @@ namespace Farmhouse_Basement_Revised
             return true;
         }
 
+        /// <summary>
+        /// Checks the given action. Currently does not work as Game2.getHook() does not work.
+        /// </summary>
+        /// <param name="tileLocation">Tile location to check action on.</param>
+        /// <param name="viewport">Current viewport</param>
+        /// <param name="who">Which Farmer</param>
+        /// <returns>true if checkaction works correctly.</returns>
         public override bool checkAction(Location tileLocation, Rectangle viewport, Farmer who)
         {
             if (Game1.player.houseUpgradeLevel > 2 ) {
                 if (mod != null)
                     mod.Monitor.Log("Successfully intercepted checkAction.", LogLevel.Debug);
+
+                // @TODO: Fix Game2.getHook()
                 return Game2.getHook().OnGameLocation_CheckAction(this, tileLocation, viewport, who, delegate {
                     if (who.IsLocalPlayer && who.currentUpgrade != null && name.Equals("Farm") && tileLocation.Equals(new Location((int)(who.currentUpgrade.positionOfCarpenter.X + 32f) / 64, (int)(who.currentUpgrade.positionOfCarpenter.Y + 32f) / 64))) {
                         if (who.currentUpgrade.daysLeftTillUpgradeDone == 1) {
@@ -92,14 +105,72 @@ namespace Farmhouse_Basement_Revised
             return base.checkAction(tileLocation, viewport, who);
         }
 
-        public bool performAction(string action, Farmer who, Location tileLocation) {
-            return false;
+        /// <summary>
+        /// Performs an action on a given tile. In this case catches the Carpenter action to inject custom code, and returns the base otherwise.
+        /// </summary>
+        /// <param name="action">What is the action that needs to be checked.</param>
+        /// <param name="who">Which Farmer initiated the action.</param>
+        /// <param name="tileLocation">Which tile is the action performed at?</param>
+        /// <returns></returns>
+        public new bool performAction(string action, Farmer who, Location tileLocation) {
+            if (action != null && who.IsLocalPlayer) {
+                string[] actionParams = action.Split(' ');
+                if (actionParams[0] == "Carpenter") {
+                    if (who.getTileY() > tileLocation.Y)
+                        carpenters(tileLocation);
+                }
+            }
+            if (Game1.player.houseUpgradeLevel > 2 ) {
+                
+            }
+            return base.performAction(action, who, tileLocation);
         }
 
-            /// <summary>
-            /// Custom code to determine if an upgrade is needed.
-            /// </summary>
-            private void houseUpgradeAccept()
+        /// <summary>
+        /// Defines the carpenter loop. This method modifies the vanilla loop to account for an additional house upgrade.
+        /// </summary>
+        /// <param name="tileLocation">The location that an action should be performed on.</param>
+        private void carpenters(Location tileLocation)
+        {
+            // @TODO: implement proper carpenter code for new upgrade
+            // (this is just copy and paster from decompiled source code)
+            if (Game1.player.currentUpgrade == null) {
+                foreach (NPC character in characters) {
+                    if (character.Name.Equals("Robin")) {
+                        if (!(Microsoft.Xna.Framework.Vector2.Distance(character.getTileLocation(), new Microsoft.Xna.Framework.Vector2((float)tileLocation.X, (float)tileLocation.Y)) > 3f)) {
+                            character.faceDirection(2);
+                            if (Game1.player.daysUntilHouseUpgrade < 0 && !Game1.getFarm().isThereABuildingUnderConstruction()) {
+                                System.Collections.Generic.List<Response> options = new System.Collections.Generic.List<Response>();
+                                options.Add(new Response("Shop", Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu_Shop")));
+                                if (Game1.IsMasterGame) {
+                                    if ((int)Game1.player.houseUpgradeLevel < 3) {
+                                        options.Add(new Response("Upgrade", Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu_UpgradeHouse")));
+                                    } else if ((Game1.MasterPlayer.mailReceived.Contains("ccIsComplete") || Game1.MasterPlayer.mailReceived.Contains("JojaMember") || Game1.MasterPlayer.hasCompletedCommunityCenter()) && (int)(Game1.getLocationFromName("Town") as StardewValley.Locations.Town).daysUntilCommunityUpgrade <= 0 && !Game1.MasterPlayer.mailReceived.Contains("pamHouseUpgrade")) {
+                                        options.Add(new Response("CommunityUpgrade", Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu_CommunityUpgrade")));
+                                    }
+                                } else if ((int)Game1.player.houseUpgradeLevel < 2) {
+                                    options.Add(new Response("Upgrade", Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu_UpgradeCabin")));
+                                }
+                                options.Add(new Response("Construct", Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu_Construct")));
+                                options.Add(new Response("Leave", Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu_Leave")));
+                                createQuestionDialogue(Game1.content.LoadString("Strings\\Locations:ScienceHouse_CarpenterMenu"), options.ToArray(), "carpenter");
+                            } else {
+                                Game1.activeClickableMenu = new StardewValley.Menus.ShopMenu(Utility.getCarpenterStock(), 0, "Robin");
+                            }
+                        }
+                        return;
+                    }
+                }
+                if (Game1.shortDayNameFromDayOfSeason(Game1.dayOfMonth).Equals("Tue")) {
+                    Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\Locations:ScienceHouse_RobinAbsent").Replace('\n', '^'));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Custom code to determine if an upgrade is needed.
+        /// </summary>
+        private void houseUpgradeAccept()
         {
             switch ((int)Game1.player.houseUpgradeLevel) {
                 case 0:
